643d2e71571a09b0722a93d315465d33a16e96eb
aaltonenlaurakaroliina <aaltonen.laura00@gmail.com>
2023-02-04 19:07:45 +0200

    nyt se toimii siel kartas ja pomppaa taakkepäin ku hyökkää

--- assets/characters/opponent/opponent.gd
+++ assets/characters/opponent/opponent.gd
@@ -1,6 +1,7 @@
 extends Character
 
-var moveSpeed : int = 200
+var staggered = false
+var moveSpeed : int = 150
 
 var sprite
 var player
@@ -12,17 +13,30 @@ func _ready():
 
 func move(delta):
 	if player:
-		var chase_direction = position.direction_to(player.position)
-		velocity = chase_direction * moveSpeed
-		move_and_slide()
+		if !staggered:
+			var chase_direction = position.direction_to(player.position)
+			velocity = chase_direction.normalized() * moveSpeed
+	move_and_slide()
 				
 func _on_chase_area_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	
 	if body.is_in_group("player"):
 		sprite.set_texture(anger)
 		player = body
 		
 	
 func _on_chase_area_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
-	player = null
-	sprite.set_texture(no_anger)
+	if body.is_in_group("player"):
+		player = null
+		sprite.set_texture(no_anger)
+		
+
+func _on_attack_area_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.is_in_group("player"):
+		attack(body)
+
+func attack(body):
+	staggered = true
+	velocity = position.direction_to(body.position) * -3 *moveSpeed
+	await get_tree().create_timer(0.2).timeout
+	moveSpeed = 100
+	staggered = false
